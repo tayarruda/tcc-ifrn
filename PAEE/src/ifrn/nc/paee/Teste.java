00f95518841969efe6ed61fc6d915ad2034e9f92
taynarruda <taynarruda@10.196.3.39>
2015-06-16 10:12:15 -0300

    Corrigindo uma alteração realizada por Laura (Antes de enviar, teste. Obrigado!!)

--- PAEE/src/ifrn/nc/paee/Teste.java
+++ PAEE/src/ifrn/nc/paee/Teste.java
@@ -2,6 +2,6 @@ package ifrn.nc.paee;
 
 public class Teste {
     public static void main(String[] args){
-      System.out.println("Oie GitHub!!!")
+      System.out.println("Oie GitHub!!!");
     }
 }
