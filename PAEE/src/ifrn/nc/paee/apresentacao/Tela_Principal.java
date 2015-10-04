14709906028a0a6165e47a5c6c6f305a56724693
tayarruda <taynarruda1@hotmail.com>
2015-10-04 15:26:30 -0300

    Iniciando a implementação da classe que iniciará a tela inicial, criando botões para acessar os experimentos.

--- PAEE/src/ifrn/nc/paee/apresentacao/Tela_Principal.java
+++ PAEE/src/ifrn/nc/paee/apresentacao/Tela_Principal.java
@@ -1,5 +1,9 @@
 package ifrn.nc.paee.apresentacao;
 
+import ifrn.nc.paee.Teste;
+import ifrn.nc.paee.servicos.bd.Experimento;
+import ifrn.nc.paee.util.CarregadorDeExperimentos;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -8,9 +12,13 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
 import java.awt.Frame;
+
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JLabel;
+
+import com.sun.xml.internal.ws.wsdl.writer.document.http.Address;
 
 public class Tela_Principal {
 
@@ -49,84 +57,33 @@ public class Tela_Principal {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
-		JPanel panel_Botoes_Experimentos = new JPanel();
-		
-		JPanel panel_Nome_Experimentos = new JPanel();
-		
 		JPanel panel_Imagem = new JPanel();
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(45)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(panel_Nome_Experimentos, GroupLayout.PREFERRED_SIZE, 179, GroupLayout.PREFERRED_SIZE)
-						.addComponent(panel_Botoes_Experimentos, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(69)
+					.addGap(294)
 					.addComponent(panel_Imagem, GroupLayout.PREFERRED_SIZE, 630, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(430, Short.MAX_VALUE))
+					.addContainerGap(438, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(24)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(panel_Imagem, GroupLayout.PREFERRED_SIZE, 443, GroupLayout.PREFERRED_SIZE)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(panel_Nome_Experimentos, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE)
-							.addGap(18)
-							.addComponent(panel_Botoes_Experimentos, GroupLayout.PREFERRED_SIZE, 290, GroupLayout.PREFERRED_SIZE)))
+					.addComponent(panel_Imagem, GroupLayout.PREFERRED_SIZE, 443, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(238, Short.MAX_VALUE))
 		);
 		
-		JLabel lblImagem = new JLabel("IMAGEM");
-		panel_Imagem.add(lblImagem);
 		
-		JLabel lblExperimentos = new JLabel("Experimentos");
-		GroupLayout gl_panel_Nome_Experimentos = new GroupLayout(panel_Nome_Experimentos);
-		gl_panel_Nome_Experimentos.setHorizontalGroup(
-			gl_panel_Nome_Experimentos.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_Nome_Experimentos.createSequentialGroup()
-					.addGap(57)
-					.addComponent(lblExperimentos))
-		);
-		gl_panel_Nome_Experimentos.setVerticalGroup(
-			gl_panel_Nome_Experimentos.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_Nome_Experimentos.createSequentialGroup()
-					.addGap(5)
-					.addComponent(lblExperimentos))
-		);
-		panel_Nome_Experimentos.setLayout(gl_panel_Nome_Experimentos);
 		
-		JButton btnEnsFund = new JButton("Ensino Fundamental");
+		Experimento[] exps = (new CarregadorDeExperimentos())
+				.getExperimentos();
+		for (Experimento experimento : exps) {
+			JButton btnExp = new JButton(experimento.getNome());
+			panel_Imagem.add(btnExp);
+			
+		}
 		
-		JButton btnEnsMedio = new JButton("Ensino M\u00E9dio");
-		
-		JButton btnEnsSuperior = new JButton("Ensino Superior");
-		GroupLayout gl_panel_Botoes_Experimentos = new GroupLayout(panel_Botoes_Experimentos);
-		gl_panel_Botoes_Experimentos.setHorizontalGroup(
-			gl_panel_Botoes_Experimentos.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_Botoes_Experimentos.createSequentialGroup()
-					.addGap(23)
-					.addGroup(gl_panel_Botoes_Experimentos.createParallelGroup(Alignment.TRAILING, false)
-						.addComponent(btnEnsSuperior, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(btnEnsMedio, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(btnEnsFund, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-					.addContainerGap(28, Short.MAX_VALUE))
-		);
-		gl_panel_Botoes_Experimentos.setVerticalGroup(
-			gl_panel_Botoes_Experimentos.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_Botoes_Experimentos.createSequentialGroup()
-					.addGap(33)
-					.addComponent(btnEnsFund, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(btnEnsMedio, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(btnEnsSuperior, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
-					.addGap(204))
-		);
-		panel_Botoes_Experimentos.setLayout(gl_panel_Botoes_Experimentos);
-		frame.getContentPane().setLayout(groupLayout);
 	}
 
 }
